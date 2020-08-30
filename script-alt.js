8f3254d16b2c8fe4c3a0cba4220add8a09c56d76
cordellbonnieux <cordellbonnieux@gmail.com>
2020-08-30 10:28:06 -0700

    The basic two player version of the game works, keeps score and allows you to reset the board or game all together. Next step is to add the A.I. and any other features I glossed over.

--- script-alt.js
+++ script-alt.js
@@ -1,8 +1,6 @@
 // open game in private self executing function
 //const game = (function(){
-// TESTING AREA
-const player1 = createPlayer("player one", true);
-const player2 = createPlayer('player two', false);
+
 // player factory function
 function createPlayer(name, turn){
     return {
@@ -22,6 +20,9 @@ function createPlayer(name, turn){
         }
     }
 };
+// Create players --> next step is to add AI!
+const player1 = createPlayer("player one", true);
+const player2 = createPlayer('player two', false); // use for local & AI?
 // test each time a position is chosen to check for winner
 function winnerTest(player){
     if (player.position.topLeft == true && player.position.topCenter && player.position.topRight ||
@@ -32,6 +33,8 @@ function winnerTest(player){
         player.position.middleLeft == true && player.position.middleCenter && player.position.middleRight ||
         player.position.topCenter == true && player.position.middleCenter && player.position.bottomCenter){
             player.score++;
+            alert(`${player.name} wins the round!`);
+            resetBoard();
         }
 };
 // create function to call when button is clicked
@@ -63,6 +66,7 @@ function buttonClick(pos){
             return;
         }
         winnerTest(player1);
+        scoreOne.innerHTML = player1.score;
     } else if (player2.turn == true){ 
         tile.innerHTML = "o";
         player2.turn = false;
@@ -89,6 +93,7 @@ function buttonClick(pos){
             return;
         }
         winnerTest(player2);
+        scoreTwo.innerHTML = player2.score;
     } else {
         return;
     }
@@ -114,7 +119,44 @@ middleRight.addEventListener('click', function(){return buttonClick(middleRight)
 bottomLeft.addEventListener('click', function(){return buttonClick(bottomLeft)});
 bottomCenter.addEventListener('click', function(){return buttonClick(bottomCenter)});
 bottomRight.addEventListener('click', function(){return buttonClick(bottomRight)});
-// Creates the prompt to ask if you will player locally or vs. AI 
+// Creates the prompt to ask if you will player locally or vs. AI
+const promptWrapper = document.getElementById('promptWrapper');
+const onePlayer = document.getElementById('computer');
+onePlayer.addEventListener('click', function(){
+    // When I build the AI, this is where I would activate it!
+});
+const twoPlayer = document.getElementById('local');
+twoPlayer.addEventListener('click', function(){
+    promptWrapper.style.cssText = "display:none;";
+});
+// Score Keeper
+const scoreOne = document.getElementById('scoreOne');
+const scoreTwo = document.getElementById('scoreTwo');
+// Reset the board (and button)
+const resetBoardButton = document.getElementById('resetBoard');
+resetBoardButton.addEventListener('click', function(){return resetBoard()}); 
+function resetBoard(){
+    topLeft.innerHTML = "", topCenter.innerHTML = "", topRight.innerHTML = "",
+    middleLeft.innerHTML = "", middleCenter.innerHTML = "", middleRight.innerHTML = "",
+    bottomLeft.innerHTML = "", bottomCenter.innerHTML = "", bottomRight.innerHTML = "";
+    player1.position.topLeft = false, player1.position.topCenter = false, player1.position.topRight = false,
+    player1.position.middleLeft = false, player1.position.middleCenter = false, player1.position.middleRight = false,
+    player1.position.bottomLeft = false, player1.position.bottomCenter = false, player1.position.bottomRight = false;
+    player2.position.topLeft = false, player2.position.topCenter = false, player2.position.topRight = false,
+    player2.position.middleLeft = false, player2.position.middleCenter = false, player2.position.middleRight = false,
+    player2.position.bottomLeft = false, player2.position.bottomCenter = false, player2.position.bottomRight = false;
+};
+// Reset the game (and button)
+const resetGameButton = document.getElementById('resetGame');
+resetGameButton.addEventListener('click', function(){return resetGame()});
+function resetGame(){
+    resetBoard();
+    player1.score = 0;
+    player2.score = 0;
+    scoreOne.innerHTML = player1.score;
+    scoreTwo.innerHTML = player2.score;
+}
+
 
 
 // closing brackets...
